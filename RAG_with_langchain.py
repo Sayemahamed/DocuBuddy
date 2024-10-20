bfb995a99b2d131d113ab31a5e3e853c8fac9d06
Sayemahamed <sayemahamed183@gmail.com>
2024-10-20 21:40:15 +0600

    after all the effort embedding is working

---
+++ RAG_with_langchain.py
@@ -0,0 +1,45 @@
+"""This module contains the code for the RAG with Langchain."""
+
+import os
+
+from langchain_ollama.llms import OllamaLLM
+from langchain_ollama.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import CSVLoader  # @type: ignore
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+# from langchain_community.chains import
+from langchain_core.documents.base import Document
+# from langchain import PromptTemplate
+# from langchain.chains import LLMChain
+
+llm = OllamaLLM(model="phi3.5")
+embedding_model = OllamaEmbeddings(model="phi3.5")
+
+
+def generate_knowledge_from_csv():
+    """Generate knowledge from a CSV file."""
+    total_data = []
+    directory = "./directory"
+    for filename in os.listdir(directory):
+        if os.path.isfile(os.path.join(directory, filename)):
+            loader = CSVLoader(
+                file_path=os.path.join(directory, filename),
+                encoding="utf-8",
+                csv_args={"delimiter": ","},
+            )
+            data: list[Document] = loader.load()
+            total_data.extend(data)
+    return total_data
+
+
+if __name__ == "__main__":
+    # Generate knowledge from a CSV file
+    print("start")
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500, chunk_overlap=0, length_function=len
+    )
+    data = splitter.split_documents(generate_knowledge_from_csv())
+    db = FAISS.from_documents(data, embedding_model)
+    print("finish")
+    FAISS.save_local(db, "db")
--- main.py
+++ main.py
@@ -1,26 +1,38 @@
 """This is the main script for the DocuBuddy app."""
 
+import os
 import textwrap
-from langchain_ollama.llms import OllamaLLM
 
 import streamlit as st
+from streamlit.runtime.uploaded_file_manager import UploadedFile
 
 
 def main() -> None:
-    llm = OllamaLLM(model="phi3.5")
-
     st.title("DocuBuddy")
     with st.sidebar:
-        st.title("DocuBuddy")
-        with st.form(key="my_form"):
-            query: str = st.sidebar.text_area(
-                label="Ask me about the video?", max_chars=50, key="query"
+        st.title("Submit your Documents")
+        with st.form(key="document_form"):
+            document: UploadedFile | None = st.file_uploader(
+                "Upload your document", type="csv"
             )
-            submit_button = st.form_submit_button(label="Submit")
-    if query != "" and query is not None:
-        answer: str = llm.invoke(query)
-        st.subheader("Answer:")
-        st.text(textwrap.fill(answer, width=85))
+            st.form_submit_button(label="Submit")
+            if document is not None:
+                # Define the directory where you want to save the file
+                directory = "./directory"
+                if not os.path.exists(directory):
+                    os.makedirs(directory)
+
+                # Save the uploaded file to the directory
+                file_path = os.path.join(directory, document.name)
+                with open(file_path, "wb") as file:
+                    file.write(document.getvalue())
+
+                st.success(f"File saved to {file_path}")
+            else:
+                st.info("No file uploaded")
+
+    st.subheader("Answer:")
+    st.text(textwrap.fill("Hello, World!", width=85))
 
 
 if __name__ == "__main__":
