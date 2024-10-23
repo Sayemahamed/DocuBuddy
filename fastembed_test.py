50466479e4a562f44bd90c96ca8d073c2ae03ccf
Sayemahamed <sayemahamed183@gmail.com>
2024-10-23 20:25:21 +0600

    tried with new embedding model

--- fastembed_test.py
+++ fastembed_test.py
@@ -2,18 +2,30 @@
 
 import os
 
+import fastembed
 from langchain_ollama.llms import OllamaLLM
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
 from langchain_community.document_loaders import CSVLoader  # @type: ignore
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from sentence_transformers import SentenceTransformer
 
+# embedding_model = fastembed.Embed(model_name="all-mpnet-base-v2")
 # from langchain_community.chains import
 # from langchain import PromptTemplate
 # from langchain.chains import LLMChain
+import pandas as pd
+
+df = pd.read_csv("./directory/customer_journey_report.csv")
+df.sort_values("Number of Published Sessions", ascending=False, inplace=True)
+df
+
+for model in fastembed.TextEmbedding().list_supported_models():
+    print(model["model"])
 
 llm = OllamaLLM(model="phi3.5")
 embeddings = FastEmbedEmbeddings()
+model = SentenceTransformer("all-mpnet-base-v2")
 
 
 def generate_knowledge_from_csv():
@@ -38,4 +50,7 @@ if __name__ == "__main__":
         chunk_size=500, chunk_overlap=0, length_function=len
     )
     data = splitter.split_documents(generate_knowledge_from_csv())
+    embeddings.model_name = "all-mpnet-base-v2"
     db = FAISS.from_documents(data, embeddings)
+    db.similarity_search("which school has Number of Published Sessions")
+    db.save_local("./db", index_name="csv")
